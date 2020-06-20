fcd23ec78f906effa82d7231d704cde6f3ea31c6
Erdem Emin Akçay <eea@jysk.com>
2020-06-20 16:10:28 +0200

    variable definition added (close #1)

--- R/mod_faithful_histogram.R
+++ R/mod_faithful_histogram.R
@@ -10,6 +10,8 @@ NULL
 
 #' @rdname mod_faithful_histogram
 #'
+#' @param variable Reactive expression yielding variable to plot the histogram
+#'
 #' @importFrom shiny NS tagList
 mod_faithful_histogram_ui <- function(id) {
   ns <- NS(id)
