ada95765b0b78b198b8d49e060568ed20b9c75bb
Erdem Emin Akçay <eea@jysk.com>
2020-06-20 17:18:23 +0200

    deploy-shinyapps.R created

---
+++ .github/deploy/deploy-shinyapps.R
@@ -0,0 +1,10 @@
+rsconnect::setAccountInfo(
+  Sys.getenv("SHINYAPPS_ACCOUNT"),
+  Sys.getenv("SHINYAPPS_TOKEN"),
+  Sys.getenv("SHINYAPPS_SECRET")
+)
+rsconnect::deployApp(
+  appName = "ShinyCICD",
+  # exclude hidden files and renv directory (if present)
+  appFiles = setdiff(list.files(), "renv")
+)
